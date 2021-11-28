bf344ae11d317d6ed3be282995df4f8fbddb3f84
Miuroku <thebestguy1101001@gmail.com>
2021-11-28 19:44:39 +0300

    feat: lab3 done

---
+++ WEB_953501_Shop/Views/Shared/Components/Menu/Default.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WEB_953501_Shop.Views.Shared.Components.Menu
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
