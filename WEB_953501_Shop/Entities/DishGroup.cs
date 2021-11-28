edf7c50062ea2a0ce966edddcb0a8f200b79a476
Miuroku <thebestguy1101001@gmail.com>
2021-11-28 22:04:53 +0300

    feat: lab5 done

---
+++ WEB_953501_Shop/Entities/DishGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_953501_Shop.Entities
+{
+    public class DishGroup
+    {
+        public int DishGroupId { get; set; }
+        public string GroupName { get; set; }
+        /// <summary>
+        /// Навигационное свойство 1-ко-многим
+        /// </summary>
+        public List<Dish> Dishes { get; set; }
+    }
+}
