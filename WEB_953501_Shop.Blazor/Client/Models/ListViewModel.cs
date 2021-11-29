b7f00ecd204ec9e4de37504111f82b9d31fee962
Miuroku <thebestguy1101001@gmail.com>
2021-11-29 21:08:18 +0300

    feat: lab8 done

---
+++ WEB_953501_Shop.Blazor/Client/Models/ListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WEB_953501_Shop.Blazor.Client.Models
+{
+    public class ListViewModel
+    {
+        [JsonPropertyName("dishId")]
+        public int DishId { get; set; } // id блюда
+        [JsonPropertyName("dishName")] public string DishName { get; set; } // название блюда
+    }
+}
